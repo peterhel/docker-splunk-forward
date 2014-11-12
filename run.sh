c4d4fd6f3c73a69bcef53033934d8badbbca8053
parallels <peter@hertigcarl.se>
2014-11-12 22:00:14 +0100

    trallalla

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
-sudo docker build -t logger ./client-logger
-sudo docker build -t splunk ./splunk-forwarder
+sudo docker ps | awk 'NR > 1 {print $1}' | xargs sudo docker stop
+sudo docker ps -a | awk 'NR > 1 {print $1}' | xargs sudo docker rm -f
 
-sudo docker run -d logger 
-sudo docker run --volumes-from logger -it splunk
+sudo docker build --no-cache -t logger ./client-logger
+sudo docker build --no-cache -t splunk ./splunk-forwarder
+
+sudo docker run -p 4500:4500 -p 4443:4443 --name logger -d logger 
+sudo docker run --volumes-from logger --name splunk -itd splunk
