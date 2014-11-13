6ce9e7fdb2b5e8fb8e96ed6b2a2be739a739ccea
parallels <peter@hertigcarl.se>
2014-11-13 09:08:09 +0100

    fixed expect script

--- run.sh
+++ run.sh
@@ -1,8 +1,15 @@
-sudo docker ps | awk 'NR > 1 {print $1}' | xargs sudo docker stop
-sudo docker ps -a | awk 'NR > 1 {print $1}' | xargs sudo docker rm -f
+#sudo docker ps | awk 'NR > 1 {print $1}' | xargs sudo docker stop
+#sudo docker ps -a | awk 'NR > 1 {print $1}' | xargs sudo docker rm -f
 
-sudo docker build --no-cache -t logger ./client-logger
-sudo docker build --no-cache -t splunk ./splunk-forwarder
+sudo docker stop splunk
+sudo docker stop logger
+sudo docker rm splunk
+sudo docker rm logger
+
+sudo docker build -t logger ./client-logger
+sudo docker build -t splunk ./splunk-forwarder
+# --no-cache
+
 
 sudo docker run -p 4500:4500 -p 4443:4443 --name logger -d logger 
 sudo docker run --volumes-from logger --name splunk -itd splunk
